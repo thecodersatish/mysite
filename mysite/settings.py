7a238b3ed89554f2e61c8f543a8f5818cd6d4079
thecodersatish <thecodersatish@gmail.com>
2022-04-06 16:55:49 +0530

    few changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
 LOGIN_REDIRECT_URL = "/dashboard"
