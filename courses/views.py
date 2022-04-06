4ac862f6777822559caadb5bc78241f0ad7e759c
thecodersatish <thecodersatish@gmail.com>
2022-04-06 15:04:57 +0530

    few  changes

--- courses/views.py
+++ courses/views.py
@@ -21,63 +21,10 @@ import datetime
 
 
 def index(request):
+    print(request.user.is_authenticated)
     if request.user.is_authenticated:
         return redirect('dashboard')
     return render(request,'index.html')
-
-def login_view(request):
-    next = request.GET.get('next', '/')
-    if request.user.is_authenticated:
-        return redirect('/dashboard')
-    form = LoginForm(request.POST or None)
-    msg = None
-    if request.method == "POST":
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                if next!=None:
-                    return redirect(next)
-                return redirect("/dashboard")
-            else:
-                msg = 'Invalid credentials'
-        else:
-            msg = 'Error validating the form'
-
-    return render(request, "accounts/login.html", {"form": form, "msg": msg})
-
-
-
-def register_user(request):
-    if request.user.is_authenticated:
-        return redirect('/dashboard')
-    msg = None
-    success = False
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            msg = 'User created - please <a href="/login">login</a>.'
-            success = True
-            return redirect("/dashboard")
-
-        else:
-            msg = 'Form is not valid'
-    else:
-        form = SignUpForm()
-
-    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
-
-
-def logout_user(request):
-    logout(request)
-    return redirect('login')
 
 
 @login_required
