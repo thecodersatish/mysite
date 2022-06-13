1d0b7b512442b1ad34da692aa46f7748f5eab91f
thecodersatish <thecodersatish@gmail.com>
2022-06-13 15:26:39 +0530

    change debug mode

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "l5074en*jgi48%6&abe$8_izjtdzi!a7%s2ludjti=)cp-f@w*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
